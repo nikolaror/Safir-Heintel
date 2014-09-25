96bfd6c5e37c7092e9823965dc88fb793639cb4f
nikolaror <dux2905@yahoo.com>
2014-09-25 17:00:25 +0200

    pretvorio json u array

--- _layout/js/ng/app.js
+++ _layout/js/ng/app.js
@@ -1,5 +1,56 @@
 var myApp = angular.module('myApp', []);
 myApp.controller('portfolioCtrl', function($scope, jsonFactory) {
+//////////////////////////////////////
+   var rawData = {
+    "0": {
+        "id": 1,
+        "title": "proizvod 55555",
+        "img_src": "_content/portfolio/220x217-5.png",
+        "url": "_content/portfolio/220x217-5.png"
+    },
+    "1": {
+        "id": 2,
+        "title": "proizvod 4404",
+        "img_src": "_content/portfolio/220x217-4.png",
+        "url": "_content/portfolio/220x217-4.png"
+    },
+    "2": {
+        "id": 3,
+        "title": "proizvod 333",
+        "img_src": "_content/portfolio/220x217-3.png",
+        "url": "_content/portfolio/220x217-3.png"
+    },
+    "3": {
+        "id": 4,
+        "title": "proizvod 222",
+        "img_src": "_content/portfolio/220x217-2.png",
+        "url": "_content/portfolio/220x217-2.png"
+    },
+    "4": {
+        "id": 5,
+        "title": "proizvod 111",
+        "img_src": "_content/portfolio/220x217-1.png",
+        "url": "_content/portfolio/220x217-1.png"
+    }
+};
+    var hasData = true;
+    var idx = 0;
+    $scope.schoolList = [];
+    while(hasData){
+        var data = rawData[idx];
+        if (data){
+            $scope.schoolList.push(data);
+            idx++;
+        }
+        else{
+            hasData=false;
+        }
+    }
+///////////////////////////////////
+
+
+
+
 
 $scope.number = 2;
 $scope.getNumber = function(num) {
@@ -7,40 +58,19 @@ $scope.getNumber = function(num) {
 }
    $scope.ukupno = {};
    $scope.portfolio ={};
-   $scope.lista ={};
-
    jsonFactory.getJSONAsync(function(results) {
     $scope.portfolio = results;
 	$scope.ukupno = Object.keys(results).length;
 	  });
-	/////////////////
-	/*jsonFactory.getSL(function($scope.portfolio) {
-			$scope.lista = $scope.portfolio;
-	  });*/
-	/*
-var rawData = {"0":{"school_name":"FAKE ELEMENTARY"},
-                         "1":{"school_name":"CENTRAL HIGH"},
-                         "2":{"school_name":"OAK RIDGE ELEMENTARY"},
-                         "3":{"school_name":"PINE MEADOW ELEMENTARY"},
-                         "4":{"school_name":"AMERICAN MIDDLE"},
-                         "5":{"school_name":"BIG SENIOR HIGH"},
-                         "6":{"school_name":"ST FRANCIS ELEMENTARY SCHOOL"}
-                         };*/
-    
-	
-	
-	
-	
-	
-	/////////////////
-	/////////////////
-	/*    $scope.currentPage = 1;
-    $scope.pageSize = 10;
-	$scope.numberOfPages=function(){
-        return Math.ceil(ukupno/pageSize);                
-    }*/
-	/////////
 });
+
+
+function myCtrl($scope){
+ 
+}
+
+
+
 myApp.factory('jsonFactory', function($http) {
   return {
     getJSONAsync: function(callback) {
