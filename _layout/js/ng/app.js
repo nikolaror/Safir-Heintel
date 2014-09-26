28d4f8703460d7d6ae60b7ea83831f31a7e0678d
nikolaror <dux2905@yahoo.com>
2014-09-26 16:30:46 +0200

    skracen text

--- _layout/js/ng/app.js
+++ _layout/js/ng/app.js
@@ -4,13 +4,12 @@ myApp.filter('slicce', function() {
   return function(arr, start, end) {
   if(arr.length > 0){
 		return (arr || []).slice(start*4, end*4+4);
-		//return arr.slice(start*4, end*4+4);
 	}
 	else{arr=[];}
   };
 });
-myApp.controller('portfolioCtrl', function($scope, jsonFactory) {
 
+myApp.controller('portfolioCtrl', function($scope, jsonFactory) {
 $scope.rowValue = 0;
 $scope.setIndex = function(num) {
 	$scope.rowValue = num;
@@ -18,25 +17,21 @@ $scope.setIndex = function(num) {
 $scope.getRowValue = function() {
 	return $scope.rowValue;
 }
-
 $scope.number = 4;
 $scope.getNumber = function(num) {
     return new Array(num);   
 }
-
-   $scope.ukupno = {};
-   $scope.portfolio ={};
-   jsonFactory.getJSONAsync(function(results) {
+$scope.ukupno = {};
+$scope.portfolio ={};
+jsonFactory.getJSONAsync(function(results) {
     $scope.portfolio = results;
 	$scope.ukupno = Object.keys(results).length;
 	  });
-
 	var hasData = true;
     var idx = 0;
     $scope.schoolList = [];	  
     while(hasData){
-        //var data = rawData[idx];////////promena
-		var data = $scope.portfolio[idx];//rawData[idx];////////promena
+		var data = $scope.portfolio[idx];
         if (data){
             $scope.schoolList.push(data);
             idx++;
@@ -54,21 +49,6 @@ myApp.factory('jsonFactory', function($http) {
 				alert("greska");
 				});
 				return callback;
-    },
-	getCustomers:function() {
-		var promise = $http({
-			method : 'POST',
-			url : '_layout/js/ng/portfolio.json'
-		}).success(function(data, status, headers, config) {
-			$log.log('Done');
-			angular.forEach(data, function(c) {
-				$log.log(c.firstName);
-			});
-			customers = data;
-			return customers;
-		});    
-
-		return promise; 
-	}
+    }
   };
 });
