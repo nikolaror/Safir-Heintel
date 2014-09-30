c9786035663f5cbb30d72d1d9a184326089db885
nikolaror <dux2905@yahoo.com>
2014-09-30 15:57:52 +0200

    listanje sa loaderom

--- _layout/js/ng/app.js
+++ _layout/js/ng/app.js
@@ -3,13 +3,13 @@ var myApp = angular.module('myApp', []);
 myApp.filter('slicce', function() {
   return function(arr, start, end) {
   if(arr.length > 0){
-		return (arr || []).slice(start*4, end*4+4);
+		return (arr || []).slice(start*4+16*(end-1),start*4+4+16*(end-1));
 	}
 	else{arr=[];}
   };
 });
 
-myApp.controller('portfolioCtrl', function($scope, jsonFactory) {
+myApp.controller('portfolioCtrl', function($scope, $location, $anchorScroll, $timeout, $q, $interval, jsonFactory) {
 $scope.rowValue = 0;
 $scope.setRowValue = function(num) {
 	$scope.rowValue = num;
@@ -17,48 +17,69 @@ $scope.setRowValue = function(num) {
 $scope.getRowValue = function() {
 	return $scope.rowValue;
 }
-////////////////////
 $scope.selected = 1;
+var stop;
+$scope.myValue = true;
+$scope.setPage = function(index) {
+	$scope.myValue = false;
+	if ( angular.isDefined(stop) ) return;
+	
+	stop = $interval(function() {
+	$scope.selected = index;
+	$location.hash('gallery');
+	$anchorScroll();
+	$scope.stopTimer();
+    }, 500);  
+}
+$scope.stopTimer = function() {
+      if (angular.isDefined(stop)) {
+        $interval.cancel(stop);
+        stop = undefined;
+		$scope.myValue = true;
+      }
+};
+/*
 $scope.setPage = function(index) {
 	$scope.selected = index;
+	$location.hash('gallery');
+	$anchorScroll();
 }
+*/
 $scope.isSelected = function(index) {
     return $scope.selected === index;
 }
-///////////////////
+
 $scope.number = 4;
+/*$scope.getNumber = function(num) {
+	$scope.arr = [];
+	for(var i = 5; i <= $scope.number; i++){
+        $scope.arr.push(i);
+    }
+	console.log("arr je:"+$scope.arr);
+    return $scope.arr;   
+}*/
 $scope.getNumber = function(num) {
     return new Array(num);   
 }
-$scope.ukupno = {};
+$scope.ukupno = Object.keys(jsonFactory.callback).length;
 $scope.portfolio ={};
-jsonFactory.getJSONAsync(function(results) {
-    $scope.portfolio = results;
+jsonFactory.getJSONAsync(function(results) { 	
+	$scope.portfolio = results;
 	$scope.ukupno = Object.keys(results).length;
-	console.log("uzimam objekat")
 });
+
 $scope.getPagesNumber = function() {
 	var pagesNo = Math.ceil($scope.ukupno/16);
-	console.log("to je "+pagesNo);
 	return new Array(pagesNo);   
 }
-/*var hasData = true;
-var idx = 0;
-$scope.schoolList = [];	  
-while(hasData){
-	var data = $scope.portfolio[idx];
-	if (data){
-		$scope.schoolList.push(data);
-		idx++;
-	}
-	else{
-		hasData=false;
-	}
-}*/
 });//////////end of controller///////
 
 myApp.factory('jsonFactory', function($http) {
+  var callback=[];
   return {
+	  callback: function(){
+		return callback;
+	},
     getJSONAsync: function(callback) {
       $http.get('_layout/js/ng/portfolio.json').success(callback).error(function(){
 				alert("greska");
@@ -67,11 +88,3 @@ myApp.factory('jsonFactory', function($http) {
     }
   };
 });
-/*
-myApp.factory('pagesFactory', function($http) {
-  return {
-    getPagesNumber: function(callback) {
-		return
-    }
-  };
-});*/
